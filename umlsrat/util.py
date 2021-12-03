4484c4483e95e00ec3baee70a4c4f1a2dcb00a7a
Russell Klopfer <rklopfer@mmm.com>
2021-12-03 12:17:58 -0500

    nfc

--- umlsrat/util.py
+++ umlsrat/util.py
@@ -3,6 +3,7 @@ from typing import Callable, Any, Dict, List, Iterable
 
 
 def group_data(data: Iterable, key_fn: Callable[[Any], Any]) -> Dict[Any, List]:
+    """https://docs.python.org/3/library/itertools.html#itertools.groupby"""
     grouped = dict()
     data = sorted(data, key=key_fn)
     for k, g in itertools.groupby(data, key_fn):
