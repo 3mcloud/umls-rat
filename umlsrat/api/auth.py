2da9f7c958965d2064885cbcea3bf147aac975c2
Russell Klopfer <rklopfer@mmm.com>
2021-12-15 23:40:10 -0500

    clean up

--- umlsrat/api/auth.py
+++ umlsrat/api/auth.py
@@ -1,42 +1,44 @@
+import functools
 import re
 
 from umlsrat.api.session import uncached_session, tgt_session
 
-uri = "https://utslogin.nlm.nih.gov"
+_auth_uri = "https://utslogin.nlm.nih.gov"
 # option 1 - username/pw authentication at /cas/v1/tickets
-# auth_endpoint = "/cas/v1/tickets/"
+# _auth_endpoint = "/cas/v1/tickets/"
 # option 2 - api key authentication at /cas/v1/api-key
-auth_endpoint = "/cas/v1/api-key"
+_auth_endpoint = "/cas/v1/api-key"
 
 _FORM_ACTION_PAT = re.compile(r'<form action="(.+?)" method="POST">')
 
 
+@functools.lru_cache(maxsize=1)
+def get_tgt(api_key: str):
+    # params = {'username': self.username,'password': self.password}
+    params = {"apikey": api_key}
+    h = {
+        "Content-type": "application/x-www-form-urlencoded",
+        "Accept": "text/plain",
+        "User-Agent": "python",
+    }
+    r = tgt_session().post(_auth_uri + _auth_endpoint, data=params, headers=h)
+    r.raise_for_status()
+
+    response_text = r.text
+
+    result = _FORM_ACTION_PAT.search(response_text).group(1)
+    assert result
+    return result
+
+
 class Authenticator(object):
     def __init__(self, api_key: str):
-        # self.username=username
-        # self.password=password
         self.api_key = api_key
         self._auth_svc = "http://umlsks.nlm.nih.gov"
-        self._auth_target = self._get_ticket_granting_ticket()
 
-    def _get_ticket_granting_ticket(self):
-        # params = {'username': self.username,'password': self.password}
-        params = {"apikey": self.api_key}
-        h = {
-            "Content-type": "application/x-www-form-urlencoded",
-            "Accept": "text/plain",
-            "User-Agent": "python",
-        }
-        r = tgt_session().post(uri + auth_endpoint, data=params, headers=h)
-        r.raise_for_status()
-
-        response_text = r.text
-
-        # <form action="https://utslogin.nlm.nih.gov/cas/v1/api-key/TGT-909652-QxfVOYJY4vH7J0fdhGtvqbDAdqwnJnYEMg7HHobnUMaFfm7Zir-cas"
-        # method="POST">
-        match = _FORM_ACTION_PAT.search(response_text)
-        result = match.group(1)
-        return result
+    @property
+    def ticket_granting_ticket(self):
+        return get_tgt(self.api_key)
 
     def get_ticket(self):
         params = {"service": self._auth_svc}
@@ -45,8 +47,11 @@ class Authenticator(object):
             "Accept": "text/plain",
             "User-Agent": "python",
         }
-        r = uncached_session().post(self._auth_target, data=params, headers=h)
-        if r.status_code != 200:
-            raise ValueError(f"Request failed: {r.content}")
+
+        tgt = self.ticket_granting_ticket
+
+        r = uncached_session().post(tgt, data=params, headers=h)
+        r.raise_for_status()
+
         st = r.text
         return st
