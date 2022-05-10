fd22a610e0163cc7eee8bd54c1be7e62540236d2
Russell Klopfer <rklopfer@mmm.com>
2022-05-10 16:27:28 -0400

    clean up

--- umlsrat/lookup/umls.py
+++ umlsrat/lookup/umls.py
@@ -10,58 +10,6 @@ from umlsrat.vocabularies import vocab_tools
 logger = logging.getLogger(os.path.basename(__file__))
 
 
-def _get_umls_concept(api: MetaThesaurus, result: Dict) -> Optional[Dict]:
-    for c_uri in (result[_] for _ in ("concept", "concepts") if _ in result):
-        concept_res = api.get_results(c_uri)
-        for c in concept_res:
-            # check for valid UI
-            if c["ui"]:
-                return c
-
-
-# def _find_umls(api: MetaThesaurus, source_res: Dict) -> Dict:
-#     umls = _get_umls_concept(api, source_res)
-#     if umls:
-#         return umls
-#
-#     # if we didn't find a UMLS concept directly, check 'SY' then 'RO' relations
-#     relations = api.get_results(source_res.get("relations"))
-#     grouped = misc.group_data(relations, lambda _: _["relationLabel"])
-#
-#     for rel_type in (
-#         "SY",
-#         "RO",
-#     ):
-#         rels = grouped.get(rel_type)
-#         if not rels:
-#             logger.info(f"'{source_res['ui']}' has no '{rel_type}' relations")
-#             continue
-#
-#         for rel in rels:
-#             rel_concept = api.get_single_result(rel["relatedId"])
-#             return _get_umls_concept(api, rel_concept)
-#
-#     raise ValueError(f"Impossible to find UMLS concept for:\n{source_res}")
-
-
-# def find_umls(api: MetaThesaurus, source_vocab: str, concept_id: str) -> Optional[str]:
-#     """
-#     Get the UMLS CUI for a concept from a source vocabulary.
-#
-#     :param api: MetaThesaurus API
-#     :param source_vocab: source vocabulary abbrev (i.e. SNOMEDCT_US)
-#     :param concept_id: source concept ID
-#     :return: UMLS CUI or None
-#     """
-#     source_res = api.get_source_concept(source_vocab, concept_id)
-#     if not source_res:
-#         logger.info(f"Concept not found: {source_vocab}/{concept_id}")
-#         return None
-#
-#     concept_res = _find_umls(api, source_res)
-#     return concept_res["ui"]
-
-
 def _term_search(api: MetaThesaurus, term: str, max_results: int) -> Dict:
     for st in ("words", "normalizedWords", "approximate"):
         for it in (
@@ -191,11 +139,10 @@ def get_cui_for(
 ) -> Optional[str]:
     """
     Get UMLS CUI for a source concept.
-    /search/current?string=9468002&inputType=sourceUi&searchType=exact&sabs=SNOMEDCT_US
 
-    :param api:
-    :param source_vocab:
-    :param concept_id:
+    :param api: MetaThesaurus
+    :param source_vocab: e.g. SNOMED
+    :param concept_id: concept ID in the source vocab
     :return: CUI or None if not found
     """
 
