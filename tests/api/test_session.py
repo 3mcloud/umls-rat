d07d146164dbd0a59e148844647679cc15b45676
Russell Klopfer <rklopfer@mmm.com>
2022-06-03 12:06:36 -0400

    fix tests

--- tests/api/test_session.py
+++ tests/api/test_session.py
@@ -1,15 +1,18 @@
+from umlsrat import const
+
+
 def test_include_flags(mt_session):
-    uri = "https://uts-ws.nlm.nih.gov/rest/search/2021AB?string=10937761000119101&sabs=SNOMEDCT_US&searchType=exact&inputType=sourceUi"
+    uri = f"https://uts-ws.nlm.nih.gov/rest/search/{const.DEFAULT_UMLS_VERSION}?string=10937761000119101&sabs=SNOMEDCT_US&searchType=exact&inputType=sourceUi"
     res = list(mt_session.get_results(uri))
 
-    include_uri = "https://uts-ws.nlm.nih.gov/rest/search/2021AB?includeObsolete=True&includeSuppressible=True&string=10937761000119101&sabs=SNOMEDCT_US&searchType=exact&inputType=sourceUi"
+    include_uri = f"https://uts-ws.nlm.nih.gov/rest/search/{const.DEFAULT_UMLS_VERSION}?includeObsolete=True&includeSuppressible=True&string=10937761000119101&sabs=SNOMEDCT_US&searchType=exact&inputType=sourceUi"
     include_res = list(mt_session.get_results(include_uri))
 
     assert len(include_res) >= len(res)
 
 
 def test_cache(mt_session):
-    concept_url = "https://uts-ws.nlm.nih.gov/rest/content/current/CUI/C0009044"
+    concept_url = f"https://uts-ws.nlm.nih.gov/rest/content/{const.DEFAULT_UMLS_VERSION}/CUI/C0009044"
     res1 = mt_session.get_single_result(concept_url)
     # modify the result
     res1["foo"] = "bar"
